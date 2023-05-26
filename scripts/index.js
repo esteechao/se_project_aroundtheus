e25e1d83a253a1389c59255698c3ca2fd46f6417
Estee C <esteechao@gmail.com>
2023-05-26 12:55:18 -0700

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,17 @@ const initialCards = [
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
 ];
+
+
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector ("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector (".modal__close");
+
+profileEditButton.addEventListener('click', () => {
+    profileEditModal.classList.add('modal_opened');
+});
+
+profileEditCloseButton.addEventListener("click", () => {
+    profileEditModal.classList.remove("modal_opened");
+});
