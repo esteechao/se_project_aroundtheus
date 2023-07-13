45fbbf5245cadc9c1c40efef8f492cacb110f510
Estee C <esteechao@gmail.com>
2023-07-12 19:39:05 -0700

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -54,6 +54,7 @@ const previewModalCloseButton =
   previewImageModal.querySelector(".modal__close");
 
 const cardAddSubmitButton = addCardModal.querySelector(".modal__button");
+
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                  form data                                     ||
 // ! ||--------------------------------------------------------------------------------||
