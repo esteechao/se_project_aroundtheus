6d3a91fdad7c14eb5318cd75fce5d9996bbb87b2
Estee C <esteechao@gmail.com>
2023-07-19 21:01:18 -0700

    wrappers

--- scripts/index.js
+++ scripts/index.js
@@ -32,6 +32,7 @@ const cardTemplate = document
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                  Wrappers                                      ||
 // ! ||--------------------------------------------------------------------------------||
+
 const cardsWrap = document.querySelector(".cards__list");
 const editProfileModal = document.querySelector("#edit-modal");
 const addCardModal = document.querySelector("#add-card-modal");
