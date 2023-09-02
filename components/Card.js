2392bc3565aaeb49e8632a9f4bf8b2d17520fa68
Estee C <esteechao@gmail.com>
2023-09-02 14:25:24 -0700

    getView

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,11 @@
 export default class Card {
     constructor ({ name, link }) {
     }
+
+    getView() {
+        //get the card view
+        //set event listners
+        //return the card
+    }
 }
+
