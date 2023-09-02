4d715494947153d3fd67f9dfce0b4400d5d36bcb
Estee C <esteechao@gmail.com>
2023-09-02 15:22:36 -0700

    refactoring

--- components/Card.js
+++ components/Card.js
@@ -7,24 +7,33 @@ export default class Card {
 
     _setEventListners() {
         //".card_like-button"
-        const likeButton = this._cardElement.querySelector('.card__like-button').addEventListner('click', () =>{
+        this._cardElement.querySelector(".card__like-button")
+        .addEventListner('click', () =>{
             this._handleLikeIcon();
-        })
+        });
         //".card_delete-button"
-        const deleteButton = this._cardElement.querySelector('card__delete-button');
+       this._cardElement.querySelector(
+        ".card__delete-button"
+        ).addEventListner('click',() => {
+        this._handleDeleteCard();
+       })
     }
 
     _handleLikeIcon(){
-        this._cardElement.querySelector('.card__like-button').classList.toggle('card__like-button_is-active');
+        this._cardElement
+        .querySelector(".card__like-button")
+        .classList.toggle("card__like-button_is-active");
     }
 
 
     getView() {
         this._cardElement = document
             .querySelector(this._cardSelector)
-            .content.querySelector('.card')
+            .content.querySelector(".card")
             .cloneNode(true);
+
         //get the card view
+        //set even listeners
         this._setEventListners();
         //return the card
     }
