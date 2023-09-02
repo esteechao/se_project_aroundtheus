50e5202f3d71ed6062608dc2c6348d32d9583728
Estee C <esteechao@gmail.com>
2023-09-02 14:45:18 -0700

    likeButton

--- components/Card.js
+++ components/Card.js
@@ -6,12 +6,17 @@ export default class Card {
     }
 
     _setEventListners() {
-        
+        //".card_like-button"
+        const likeButton = this._cardElement.querySelector('.card_like-button');
+        //".card_delete-button"
     }
 
     getView() {
+        this._cardElement = document
+            .querySelector(this._cardSelector)
+            .cloneNode(true);
         //get the card view
-        this._setEventListners()
+        this._setEventListners();
         //return the card
     }
 }
