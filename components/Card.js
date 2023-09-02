32d06134b71d3cc80ce5f458e9cb9689dfb1cd2f
Estee C <esteechao@gmail.com>
2023-09-02 14:54:26 -0700

    cardDeleteButton

--- components/Card.js
+++ components/Card.js
@@ -7,13 +7,15 @@ export default class Card {
 
     _setEventListners() {
         //".card_like-button"
-        const likeButton = this._cardElement.querySelector('.card_like-button');
+        const likeButton = this._cardElement.querySelector('.card__like-button');
         //".card_delete-button"
+        const deleteButton = this._cardElement.querySelector('card__delete-button');
     }
 
     getView() {
         this._cardElement = document
             .querySelector(this._cardSelector)
+            .content.querySelector('.card')
             .cloneNode(true);
         //get the card view
         this._setEventListners();
