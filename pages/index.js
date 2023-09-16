de32ae5d138494174e5e46a038fd0af8f6d8e1f2
Estee C <esteechao@gmail.com>
2023-09-16 12:22:35 -0700

    card validation

--- pages/index.js
+++ pages/index.js
@@ -59,6 +59,21 @@ const previewModalCloseButton =
   previewImageModal.querySelector(".modal__close");
 
 const cardAddSubmitButton = addCardModal.querySelector(".modal__button");
+
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+const addCardFormEl = document.querySelector('add-card-form');
+const addCardFormValidtor = new FormValidator(config, addCardFormEl);
+addCardFormValidtor.enableValidation();
+
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                  form data                                     ||
 // ! ||--------------------------------------------------------------------------------||
