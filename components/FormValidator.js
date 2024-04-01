88f507ee5d9ecb68454971deccc1777fd08bf5d9
Estee C <esteechao@gmail.com>
2024-03-31 21:59:38 -0700

    updates

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -7,7 +7,6 @@ export default class FormValidator {
         this._settings.submitButtonSelector
       };
     }
-    
     _showInputError(inputElement, errorMessage) {
       const errorElement = this._formElement.querySelector(
         `#${inputElement.id}-error`
